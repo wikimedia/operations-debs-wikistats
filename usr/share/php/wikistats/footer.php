9c97c56df4ebf02bcd6bea2f918a104e1889c30e
Daniel Zahn <dzahn@wikimedia.org>
2017-07-21 22:06:49 -0700

    footer: rm W3C icons and XHTML/CSS claims, use NuHTML validator
    These files have been removed, are non-free and nowadays the document
    doesn't validate as XHTML strict anymore. (it used to).
    
    Also CSS isn't valid anymore since we use Bootstrap instead of homebrew.
    
    Remove those icons and claims but still link to validator. That uses
    the NuHTML validator once the doc type gets changed here.
    
    Change-Id: I15efad937713fcc6c9ffe3d33f0720c2cea88b4c

--- usr/share/php/wikistats/footer.php
+++ usr/share/php/wikistats/footer.php
@@ -8,8 +8,6 @@ $selfurl=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; $selfurl=str_replace("&"
 <li>In use on: <a class="foot" href="<?php echo "$wikipage"; ?>"><?php echo "$wikipage"; ?></a></li>
 <li><a class="foot" href="index.php">Back to Index</a></li></ul>
 <a class="foot" href="http://validator.w3.org/check?uri=<?php echo "http://".$selfurl; ?>">
-<img style="border:0;width:60px;" src="./images/valid-xhtml10-blue.png" alt="Valid XHTML 1.0 Strict" /></a>
-<a class="foot" href="http://jigsaw.w3.org/css-validator/validator?uri=<?php echo "http://".$selfurl; ?>">
-<img style="border:0;width:60px;" src="./images/vcss-blue.png" alt="Valid CSS!" /></a>
+validate html</a>
 <br />
 <?php echo "Last modified: " . date( "F d Y - H:i:s", getlastmod() ); ?>
