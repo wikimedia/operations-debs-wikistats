19d4a3d9b630aa62e5c5176d2323aa829c914cfc
Daniel Zahn <dzahn@wikimedia.org>
2020-09-01 16:02:26 -0700

    turn footer links into list items
    Change-Id: Ie3a7c3490bb726c0a1c625dee34603b2fdeb0235

--- usr/share/php/wikistats/footer.php
+++ usr/share/php/wikistats/footer.php
@@ -7,9 +7,14 @@ $wikiurl=str_replace(".php","w.php",$selfurl);
 <ul><li><a class="foot" href="https://<?php echo "$wikiurl"; ?>">Table in Mediawiki Syntax</a></li>
 <li>In use on: <a class="foot" href="<?php echo "$wikipage"; ?>"><?php echo "$wikipage"; ?></a></li>
 <li><a class="foot" href="index.php">Back to Index</a></li></ul>
+
+<ul><li>
 <a class="foot" href="http://validator.w3.org/check?uri=<?php echo "http://".$selfurl; ?>">
 validate html</a>
-<br />
-<a class="foot" href="https://gerrit.wikimedia.org/r/admin/repos/operations/puppet">source code (puppet)</a> (sets up a wikistats instance, see role::wikistats::instance, profile::wikistats, modules/wikistats)<br />
+</li><li>
+<a class="foot" href="https://gerrit.wikimedia.org/r/admin/repos/operations/puppet">source code (puppet)</a> (sets up a wikistats instance, see role::wikistats::instance, profile::wikistats, modules/wikistats)
+</li><li>
 <a class="foot" href="https://gerrit.wikimedia.org/r/admin/repos/operations/debs/wikistats">source (PHP)</a> (application code, not really a .deb anymore)<br />
+</li><li>
 <?php echo "Last modified: " . date( "F d Y - H:i:s", getlastmod() ); ?>
+</li></ul>
