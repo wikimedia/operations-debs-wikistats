0eff8a0c08fddfa518248c11b07f5b5d47125ef9
Daniel Zahn <dzahn@wikimedia.org>
2020-09-01 15:44:52 -0700

    fix links in footer to tables in wiki syntax
    Change-Id: Ia368da49af0b0759f73e46d80566da3c9b3dcde9

--- usr/share/php/wikistats/footer.php
+++ usr/share/php/wikistats/footer.php
@@ -1,10 +1,10 @@
 <?php
-#$wikioutput=$listtable."_wiki.php";
-$wikioutput="";
 $wikipage="";
 $selfurl=$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; $selfurl=str_replace("&","&amp;",$selfurl);
+$wikiurl=str_replace(".php","w.php",$selfurl);
+
 ?>
-<ul><li><a class="foot" href="<?php echo "$wikioutput"; ?>">Table in Mediawiki Syntax</a></li>
+<ul><li><a class="foot" href="https://<?php echo "$wikiurl"; ?>">Table in Mediawiki Syntax</a></li>
 <li>In use on: <a class="foot" href="<?php echo "$wikipage"; ?>"><?php echo "$wikipage"; ?></a></li>
 <li><a class="foot" href="index.php">Back to Index</a></li></ul>
 <a class="foot" href="http://validator.w3.org/check?uri=<?php echo "http://".$selfurl; ?>">
